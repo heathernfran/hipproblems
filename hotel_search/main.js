5e684a6deebe6753ae36b6d8dc85dd4a51112a09
Heather Frantisak <hfrantisak@mvnp.com>
2017-06-19 15:13:39 -1000

    Iterate over all providers, getting data results for each provider.

--- hotel_search/main.js
+++ hotel_search/main.js
@@ -1,32 +1,42 @@
 const http = require('http');
 
-const url = 'http://localhost:9000/scrapers/Hilton';
+const providers = ['Expedia', 'Orbitz', 'Priceline', 'Travelocity', 'Hilton'];
 
-http.get(url, (response) => {
-  const { statusCode } = response;
-  
-  let error;
-  if (statusCode != 200) {
-    error = new Error(`Request failed. Status Code: ${statusCode}`);
-  }
-  if (error) {
-    console.log(`Error occurred: ${error}`);
-    response.resume();
-    return;
-  }
-  
-  response.setEncoding('utf8');
-  let rawData = '';
-  response.on('data', (chunk) => { rawData += chunk; });
-  response.on('end', () => {
-    try {
-      const parsedData = JSON.parse(rawData);
-      console.log(parsedData);
-    } catch (e) {
-      console.log(`Error on getting data: ${e}`)
-    }
-  });
-  
-}).on('error', (e) => {
-  console.log(`Got error: ${e.message}`);
+providers.forEach((item) => {
+  getProviderData(item);
 });
+
+function getProviderData(url) {
+  const localUrl = 'http://localhost:9000/scrapers/';
+
+  let concatUrl = localUrl.concat(url);
+
+  http.get(concatUrl, (response) => {
+    const { statusCode } = response;
+    
+    let error;
+    if (statusCode != 200) {
+      error = new Error(`Request failed. Status Code: ${statusCode}`);
+    }
+    if (error) {
+      console.log(`Error occurred: ${error}`);
+      response.resume();
+      return;
+    }
+    
+    response.setEncoding('utf8');
+    let rawData = '';
+    response.on('data', (chunk) => { rawData += chunk; });
+    response.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+      } catch (e) {
+        console.log(`Error on getting data: ${e}`)
+      }
+    });
+    
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`);
+  });
+}
